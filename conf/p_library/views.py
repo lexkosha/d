bc338ece0816af031cbe2c1a9a46b6572fd85085
Lex Kosha <lex.kosha@gmail.com>
2020-05-11 23:08:02 +0300

    v 0.0.3

--- conf/p_library/views.py
+++ conf/p_library/views.py
@@ -4,12 +4,12 @@ from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from django.forms import formset_factory
 
-
 # Create your views here.
 from django.views.generic.base import View
 
 from .models import Author, Book, PublishingHouse
-from .forms import AuthorForm
+from .forms import AuthorForm, BookForm
+
 
 # class HomeView(View):
 #     """Главная страница сайта"""
@@ -37,35 +37,50 @@ class AuthorList(ListView):
     template_name = 'main/authors_list.html'
 
 
-# def author_create_many(request):
-#     AuthorFormSet = formset_factory(AuthorForm, extra=2)#  Первым делом, получим класс, который будет создавать наши формы. Обратите внимание на параметр `extra`, в данном случае он равен двум, это значит, что на странице с несколькими формами изначально будет появляться 2 формы создания авторов.
-#     if request.method == 'POST':#  Наш обработчик будет обрабатывать и GET и POST запросы. POST запрос будет содержать в себе уже заполненные данные формы
-#         author_formset = AuthorFormSet(request.POST, request.FILES, prefix='authors')#  Здесь мы заполняем формы формсета теми данными, которые пришли в запросе. Обратите внимание на параметр `prefix`. Мы можем иметь на странице не только несколько форм, но и разных формсетов, этот параметр позволяет их отличать в запросе.
-#         if author_formset.is_valid():#  Проверяем, валидны ли данные формы
-#             for author_form in author_formset:
-#                 author_form.save()#  Сохраним каждую форму в формсете
-#             return HttpResponseRedirect(reverse_lazy('p_library:author_list'))#  После чего, переадресуем браузер на список всех авторов.\
-#         else:#  Если обработчик получил GET запрос, значит в ответ нужно просто "нарисовать" формы.
-#             author_formset = AuthorFormSet(prefix='authors')#  Инициализируем формсет и ниже передаём его в контекст шаблона.
-#         return render(request, 'main/manage_authors.html', {'author_formset': author_formset})
+def books_authors_create_many(requests):
+    AuthorFormSet = formset_factory(AuthorForm, extra=2)
+    BookFormSet = formset_factory(BookForm, extra=2)
+    if requests.method == 'POST':
+        author_formset = AuthorFormSet(requests.POST, requests.FILES, prefix='authors')
+        book_formset = BookFormSet(requests.POST, requests.FILES, prefix='books')
+        if author_formset.is_valid() and book_formset.is_valid():
+            for author_form in author_formset:
+                author_form.save()
+            for book_form in book_formset:
+                book_form.save()
+            return HttpResponseRedirect(reverse_lazy('p_library:author_list'))
+    else:
+        author_formset = AuthorFormSet(prefix='authors')
+        book_formset = BookFormSet(prefix='books')
+        context = {
+            'author_formset': author_formset,
+            'book_formset': book_formset,
+        }
+        return render(requests, 'main/manage_books_authors.html', context)
+
 
 def author_create_many(request):
-    AuthorFormSet = formset_factory(AuthorForm, extra=2)  #  Первым делом, получим класс, который будет создавать наши формы. Обратите внимание на параметр `extra`, в данном случае он равен двум, это значит, что на странице с несколькими формами изначально будет появляться 2 формы создания авторов.
-    if request.method == 'POST':  #  Наш обработчик будет обрабатывать и GET и POST запросы. POST запрос будет содержать в себе уже заполненные данные формы
-        author_formset = AuthorFormSet(request.POST, request.FILES, prefix='authors')  #  Здесь мы заполняем формы формсета теми данными, которые пришли в запросе. Обратите внимание на параметр `prefix`. Мы можем иметь на странице не только несколько форм, но и разных формсетов, этот параметр позволяет их отличать в запросе.
-        if author_formset.is_valid():  #  Проверяем, валидны ли данные формы
+    AuthorFormSet = formset_factory(AuthorForm, extra=2)  # Первым делом, получим класс, который будет создавать наши формы. Обратите внимание на параметр `extra`, в данном случае он равен двум, это значит, что на странице с несколькими формами изначально будет появляться 2 формы создания авторов.
+    if request.method == 'POST':  # Наш обработчик будет обрабатывать и GET и POST запросы. POST запрос будет содержать в себе уже заполненные данные формы
+        author_formset = AuthorFormSet(request.POST, request.FILES,
+                                       prefix='authors')  # Здесь мы заполняем формы формсета теми данными, которые пришли в запросе. Обратите внимание на параметр `prefix`. Мы можем иметь на странице не только несколько форм, но и разных формсетов, этот параметр позволяет их отличать в запросе.
+        if author_formset.is_valid():  # Проверяем, валидны ли данные формы
             for author_form in author_formset:
-                author_form.save()  #  Сохраним каждую форму в формсете
-            return HttpResponseRedirect(reverse_lazy('p_library:author_list'))  #  После чего, переадресуем браузер на список всех авторов.
-    else:  #  Если обработчик получил GET запрос, значит в ответ нужно просто "нарисовать" формы.
-        author_formset = AuthorFormSet(prefix='authors')  #  Инициализируем формсет и ниже передаём его в контекст шаблона.
+                author_form.save()  # Сохраним каждую форму в формсете
+            return HttpResponseRedirect(
+                reverse_lazy('p_library:author_list'))  # После чего, переадресуем браузер на список всех авторов.
+    else:  # Если обработчик получил GET запрос, значит в ответ нужно просто "нарисовать" формы.
+        author_formset = AuthorFormSet(
+            prefix='authors')  # Инициализируем формсет и ниже передаём его в контекст шаблона.
     return render(request, 'main/manage_authors.html', {'author_formset': author_formset})
+
 
 def redaction(requests):
     context = {
         'redaction': PublishingHouse.objects.all(),
     }
-    return render(requests,'main/redactions.html', context)
+    return render(requests, 'main/redactions.html', context)
+
 
 def index(requests):
     context = {'book': Book.objects.all(),
@@ -107,5 +122,3 @@ def book_decrement(request):
         return redirect('/index/')
     else:
         return redirect('/index/')
-
-
